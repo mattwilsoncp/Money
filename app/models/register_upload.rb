fc039981f2a084ba2df601d8997c2b4d0697dcff
Matt Wilson <mattwilsoncp16@gmail.com>
2017-07-23 21:46:57 -0400

    Fixing register upload show page

--- app/models/register_upload.rb
+++ app/models/register_upload.rb
@@ -2,15 +2,15 @@ require 'csv'
 
 class RegisterUpload < ApplicationRecord
   belongs_to :register
+  has_many :register_upload_entries
   mount_uploader :external_file, ExternalFileUploader
 
   def process_file
-    register_id = self.register_id
     CSV.foreach(self.external_file.file.file, :headers => true) do |row|
       data = row.to_hash
       begin
-        re = RegisterEntry.new
-        re.register_id = register.id
+        re = RegisterUploadEntry.new
+        re.register_upload_id = self.id
         re.description = data['Description']
         re.category_id = 1
         re.entry_date = Chronic.parse data['Posting Date']
